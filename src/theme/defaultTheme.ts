977cbdcbadf9eb03e720ceb9d197c34ff7c4a962
hetali-jarvisbitz <hetali.shah@jarvisbitz.com>
2021-09-29 17:10:00 +0530

    Change dark and light mode color of header and event text

--- src/theme/defaultTheme.ts
+++ src/theme/defaultTheme.ts
@@ -1,5 +1,8 @@
+import { Appearance } from 'react-native'
+
 import { ThemeInterface } from './ThemeInterface'
 
+export const isDark = Appearance.getColorScheme() === 'dark'
 export const defaultTheme: ThemeInterface = {
   isRTL: false,
   palette: {
@@ -14,15 +17,15 @@ export const defaultTheme: ThemeInterface = {
       200: '#eeeeee',
       300: '#e0e0e0',
       // 400: '#bdbdbd',
-      500: '#9e9e9e',
+      500: isDark ? '#f5f5f5' : '#9e9e9e',
       // 600: '#757575',
       // 700: '#616161',
-      800: '#424242',
+      800: isDark ? '#f5f5f5' : '#424242',
       // 900: '#212121',
     },
   },
   eventCellOverlappings: [
-    { main: '#E26245', contrastText: '#fff' }, // orange
+    { main: '#E26245', contrastText: '#f5f5f5' }, // orange
     { main: '#4AC001', contrastText: '#fff' }, // green
     { main: '#5934C7', contrastText: '#fff' }, // purple
   ],
